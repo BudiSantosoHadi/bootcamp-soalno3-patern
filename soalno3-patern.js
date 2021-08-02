70774aec1d90f634e94c73411993e5894b4e86b6
atifnaufalh <atifnaufal@gmail.com>
2021-08-02 13:54:42 +0700

    first commit

---
+++ soalno3-patern.js
@@ -0,0 +1,67 @@
+var pola = '';
+
+for(var A = '0'; A < 8 ; A++){
+    pola += '+  ';
+}
+pola += '\n';
+
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var B = '0'; B < A; B++){
+    if(B % 3 == 2){
+        pola += '+  ';
+    }else{
+        pola += '=  ';
+    
+    }
+}
+pola += '\n';
+for(var A = '0'; A < 8 ; A++){
+    pola += '+  ';
+}
+pola += '\n'
+console.log(pola);
+
